c0ae8a203428d6afa14a1bd5370eb94f00cf13ec
Dancode-188 <danbitengo@gmail.com>
2024-10-10 12:23:03 +0300

    Refactor addUser method in SocialNetwork class Refactor follow method in SocialNetwork class Refactor getFollows method in SocialNetwork class Refactor getFollowers method in SocialNetwork class Refactor getRecommendedFollows method in SocialNetwork class

--- social.js
+++ social.js
@@ -1,6 +1,5 @@
 // Implement the SocialNetwork class here
 class SocialNetwork {
-
   constructor() {
     this.users = {};
     this.follows = {};
@@ -8,27 +7,63 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++;
+    this.users[this.currentID] = { id: this.currentID, name: name };
+    this.follows[this.currentID] = new Set();
+    return this.currentID;
   }
 
   getUser(userID) {
-    // Your code here
+    return this.users[userID] || null;
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (this.users[userID1] && this.users[userID2]) {
+      this.follows[userID1].add(userID2);
+      return true;
+    }
+    return false;
   }
 
   getFollows(userID) {
-    // Your code here
+    const follows = {};
+    this.follows[userID].forEach((followedID) => {
+      follows[followedID] = true;
+    });
+    return follows;
   }
 
   getFollowers(userID) {
-    // Your code here
+    const followers = new Set();
+    Object.keys(this.follows).forEach((followerID) => {
+      if (this.follows[followerID].has(userID)) {
+        followers.add(parseInt(followerID));
+      }
+    });
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here
+    let recommended = new Set();
+    let visited = new Set([userID]); // Mark the starting user as visited
+    let current = new Set(this.follows[userID]); // Start with direct follows
+    visited = new Set([...visited, ...current]); // Mark direct follows as visited so they are not recommended immediately
+
+    for (let i = 0; i < degrees; i++) {
+      let next = new Set();
+      for (let id of current) {
+        for (let followedID of this.follows[id]) {
+          if (!visited.has(followedID)) {
+            visited.add(followedID); // Mark as visited
+            next.add(followedID); // Queue for next degree
+            recommended.add(followedID); // Recommend this user
+          }
+        }
+      }
+      current = next; // Move to the next degree
+    }
+
+    return Array.from(recommended);
   }
 }
 
